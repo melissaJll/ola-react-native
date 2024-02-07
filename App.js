7da8980002215ac3421fff6f4f220803bbe25880
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-02-07 18:48:58 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StatusBar, Text, View } from "react-native";
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="light-content" />
+      <StatusBar backgroundColor="lightgreen" barStyle="dark-content" />
       <View>
         <Text>Olá React Native!</Text>
       </View>
